7ccf468d249e66391aba4eff49f40d30f2b72eb1
LiJinQ <1464435332@qq.com>
2018-08-03 19:02:24 +0800

    新增了empinfo的Dao层增删改查方法 empinfo接口

--- src/main/java/com/ambow/dao/EmpinfoDao.java
+++ src/main/java/com/ambow/dao/EmpinfoDao.java
@@ -1,5 +1,14 @@
 package com.ambow.dao;
 
-public interface EmpinfoDao {
+import java.util.List;
 
+import com.ambow.pojo.Empinfo;
+
+public interface EmpinfoDao {
+	public void newEmpInfo(Empinfo empinfo);
+	public void updateEmpInfo(Empinfo empinfo);
+	public void deleteEmpInfoById(int empid);
+	public Empinfo getEmpinfoById(int empid);
+	public Empinfo getEmpinfoByLoginName(String loginName);
+	public List<Empinfo> getAllEmpinfo();
 }
--- src/main/java/com/ambow/sercice/EmpinfoService.java
+++ src/main/java/com/ambow/sercice/EmpinfoService.java
@@ -1,5 +1,35 @@
 package com.ambow.sercice;
 
-public interface EmpinfoService {
+import com.ambow.pojo.Empinfo;
 
+public interface EmpinfoService {
+	/**
+	 * 从Controller接受一个empinfo实体，验证登陆功能。
+	 * @param empinfo
+	 * @return
+	 * 登陆成功：true
+	 * 失败：false
+	 */
+	public boolean login(Empinfo empinfo);
+	/**
+	 * 新增员工信息
+	 * @param empinfo
+	 */
+	public void newEmpinfo(Empinfo empinfo);
+	/**
+	 * 修改员工信息
+	 * @param empinfo
+	 */
+	public void updateEmpinfo(Empinfo empinfo);
+	/**
+	 * 通过empid删除员工信息
+	 * @param id
+	 */
+	public void deleteEmpinfoById(int empid);
+	/**
+	 * 通过empid获得一个员工实体
+	 * @param empid
+	 * @return
+	 */
+	public Empinfo getEmpinfoById(int empid);
 }
